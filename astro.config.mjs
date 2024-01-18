8e32b260b60329da0a3406bd0c03f553572f7dbf
Anna Murphy <anna_murphy1@brown.edu>
2024-01-18 10:16:20 -0500

    feat: deploy to pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,7 @@
 import { defineConfig } from "astro/config";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+    site: "https://anna-murphy.github.io",
+    base: "/blog",
+});
